b11625c69ccd173171b287835e1778883b36055e
d-w-moore <dmoore@renci.org>
2021-10-23 12:32:14 -0400

    add module

---
+++ mymodule.py
@@ -0,0 +1,20 @@
+
+class myClass(object):
+    """class docstring"""
+
+    def __init__(self, x = 0): 
+        """
+        This is the myClass object constructor.
+
+        An elaboration is as follows. 'self' is the object being constructed.
+        x is a generic parameter for demonstration.
+
+        Parameters
+        ----------
+        x:
+        a value which will be stored as an attribute
+        """
+
+        self.x = x
+
+
